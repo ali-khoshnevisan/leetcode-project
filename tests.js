a3c2ec7a78bf54e47781fcfaa8229e2ab7d3025d
ali khoshnevisan <ali.kh.3437@gmail.com>
2023-03-04 04:17:38 +0330

    writing tests

--- tests.js
+++ tests.js
@@ -1,11 +1,12 @@
-
 const chai = window.chai
 const expect = chai.expect
 
 describe('msToTime', () => {
   it('pass', () => {
     expect(msToTime(1356400)).to.deep.equal('00:22:36')
-    // expect(msToTime()).to.deep.equal()
-    // expect(msToTime()).to.deep.equal()
+    expect(msToTime(56890021)).to.deep.equal('15:48:10')
+    expect(msToTime(3265)).to.deep.equal('00:00:03')
+    expect(msToTime(3564642620)).to.deep.equal('41 day(s) and 06:10:42')
+    expect(msToTime(536890021)).to.deep.equal('6 day(s) and 05:08:10')
   })
 })
